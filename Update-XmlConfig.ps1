229447e09af1b5d80314174dd4261fd55e434a65
Tim Schindler <tim@iosharp.com>
2022-08-07 11:48:10 +0200

    fix update-xmlconfig script

--- Update-XmlConfig.ps1
+++ Update-XmlConfig.ps1
@@ -4,7 +4,7 @@
 )
 
 $XmlConfig = [xml](Get-Content -Path $File)
-foreach ($Parameter in $ParameterNameValueHashtable) {
+foreach ($Parameter in $ParameterNameValueHashtable.GetEnumerator()) {
     $Element = $XmlConfig.SelectSingleNode("//Parameter[@Name='$($Parameter.Name)']")
     $Element.Value = $($Parameter.Value)
 }
